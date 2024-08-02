ebcf7135bb6f524bbe7efcc6f7c3e092a01b8f1b
Rahul <rahulrameshr25@gmail.com>
2024-08-02 16:09:20 +0100

    this is my first commit

--- app.py
+++ app.py
@@ -6,17 +6,17 @@ from urllib.request import urlopen as uReq
 
 app = Flask(__name__)
 
-@app.route('/',methods=['GET'])  # route to display the home page
+@app.route('/',methods=['GET'])  # route to display the home page #/- local port sendind through url tring to call homepage function
 @cross_origin()
 def homePage():
-    return render_template("index.html")
+    return render_template("index.html")# render a template. tryng to showcase index.html
 
 @app.route('/review',methods=['POST','GET']) # route to show the review comments in a web UI
-@cross_origin()
-def index():
+@cross_origin()# whenever i want to deploy in a cloud server. if we didnt do this people out side my locationmeans outside country cannot access it
+def index():#at review path we are executing this
     if request.method == 'POST':
         try:
-            searchString = request.form['content'].replace(" ","")
+            searchString = request.form['content'].replace(" ","") #try to access content part of this form
             flipkart_url = "https://www.flipkart.com/search?q=" + searchString
             uClient = uReq(flipkart_url)
             flipkartPage = uClient.read()
@@ -24,8 +24,8 @@ def index():
             flipkart_html = bs(flipkartPage, "html.parser")
             bigboxes = flipkart_html.findAll("div", {"class": "_1AtVbE col-12-12"})
             del bigboxes[0:3]
-            box = bigboxes[0]
-            productLink = "https://www.flipkart.com" + box.div.div.div.a['href']
+            box = bigboxes[0]# we are going to perticular item
+            productLink = "https://www.flipkart.com" + box.div.div.div.a['href']# inside of  inside of inside
             prodRes = requests.get(productLink)
             prodRes.encoding='utf-8'
             prod_html = bs(prodRes.text, "html.parser")
@@ -40,7 +40,7 @@ def index():
             for commentbox in commentboxes:
                 try:
                     #name.encode(encoding='utf-8')
-                    name = commentbox.div.div.find_all('p', {'class': '_2sc7ZR _2V5EHH'})[0].text
+                    name = commentbox.div.div.find_all('p', {'class': '_2sc7ZR _2V5EHH'})[0].text #first command box inside that classs
 
                 except:
                     name = 'No Name'
